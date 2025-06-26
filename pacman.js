5acc3d5e80b98cd8d312d44d4171b26b71711f28
Asad <hellohello51981@gmail.com>
2025-06-26 23:11:07 +0500

    Add JS - Creating ghost

--- pacman.js
+++ pacman.js
@@ -62,3 +62,20 @@ createBoard();
 let pacmanCurrentPosition = 490;
 
 squares[pacmanCurrentPosition].classList.add('pacman');
+
+
+
+
+
+
+
+class Ghost{
+    constructor(className, startIndex, speed){
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+        this.currentIndex = startIndex
+        this.isScared = false
+        this.timerId = NaN
+    }
+  }
